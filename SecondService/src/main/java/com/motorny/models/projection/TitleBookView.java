26b578ed76d2b5a95ef546a8ddf7a548756f3b6c
Sergey Motorny <motorny0919@gmail.com>
2024-06-21 20:00:31 +0300

    adding a projection for requesting to receive books by userId

---
+++ SecondService/src/main/java/com/motorny/models/projection/TitleBookView.java
@@ -0,0 +1,16 @@
+package com.motorny.models.projection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TitleBookView {
+
+    private String fullName;
+    private String title;
+}
