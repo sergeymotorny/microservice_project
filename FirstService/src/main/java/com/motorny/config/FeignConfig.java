fdeee58f0f6f259e4b35fce240f29e784e134bd9
Sergey Motorny <motorny0919@gmail.com>
2024-06-26 17:05:48 +0300

    refactoring code

--- FirstService/src/main/java/com/motorny/config/FeignConfig.java
+++ FirstService/src/main/java/com/motorny/config/FeignConfig.java
@@ -1,6 +1,7 @@
 package com.motorny.config;
 
 import com.motorny.client.ClientErrorDecoder;
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,12 @@ public class FeignConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new ClientErrorDecoder();
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return template -> {
+            template.header("auth", "let_me_in");
+        };
     }
 }
